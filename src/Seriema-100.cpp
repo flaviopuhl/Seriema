a47b7e4e32ce2ed76edd2dbc862d9f237bbe1f07
Flávio Luiz Puhl Junior <flavio_puhl@hotmail.com>
2022-05-13 15:04:29 -0300

    first commit

--- src/Seriema-100.cpp
+++ src/Seriema-100.cpp
@@ -80,7 +80,7 @@ Upload settings
  *| Constants declaration                                                                |
  *+--------------------------------------------------------------------------------------+ */
  
-const char *ssid =  "CasaDoTheodoro1";                 // name of your WiFi network
+const char *ssid =  "CasaDoTheodoro11";                 // name of your WiFi network
 const char *password =  "09012011";                   // password of the WiFi network
 
 const char *ID = "SeriemaDev";                        // Name of our device, must be unique
